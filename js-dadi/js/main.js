d9713b19a3a2e4bf7b7a5dca1fda4e11dee78d05
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-12 16:11:37 +0100

    feat: dadi js

--- js-dadi/js/main.js
+++ js-dadi/js/main.js
@@ -15,7 +15,25 @@ const finalResult = document.getElementById("result");
 // ASSEGNO I NUMERI RANDOM A DUE COSTANTI
 
 const userNumber = Math.floor(Math.random() * 6 + 1);
-console.log(userNumber);
+console.log("numero Utente " + userNumber);
 
 const computerNumber = Math.floor(Math.random() * 6 + 1);
-console.log(computerNumber);
+console.log("numero Computer " + computerNumber);
+
+// RENDO NOTO ALL'UTENTE DEL SUON NUMERO
+userNumberResult.innerText = `Il numero generato randomicamente per te è: ${userNumber}`;
+
+// ASCOLTO L'EVENTO DEL BTN
+resultBtn.addEventListener("click", function () {
+  computerNumberResult.innerText = `Il numero generato randomicamente per il computer è: ${computerNumber}`;
+  let result = "";
+  //   CONDIZIONE PER CONTROLLARE LA GIOCATA
+  if (userNumber > computerNumber) {
+    result = "HAI VINTO!!";
+  } else if (userNumber == computerNumber) {
+    result = "PAREGGIO :/";
+  } else {
+    result = "HAI PERSO :(";
+  }
+  finalResult.innerText = result;
+});
