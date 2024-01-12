3bfcfeb8a3a3ca146f1e0d1bbb15d73a3e61c2b3
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-12 19:31:45 +0100

    feat: js mail

--- js-mail/js/main.js
+++ js-mail/js/main.js
@@ -1,1 +1,33 @@
-console.log("js OK");
+dbMail = [
+  "antoniodibari@gmail.com",
+  "mariorossi@gmail.com",
+  "giuliorossi@gmail.com",
+  "marcorossi@gmail.com",
+  "luigibianchi@gmail.com",
+  "rossimario@gmail.com",
+  "rossigiulio@gmail.com",
+  "dibariantonio@gmail.com",
+  "rossimarco@gmail.com",
+  "bianchiluigi@gmail.com",
+];
+
+const userMailInput = document.getElementById("mail-utente");
+const validateBtn = document.getElementById("validate-btn");
+const result = document.getElementById("result");
+let userMail;
+
+validateBtn.addEventListener("click", function () {
+  userMail = userMailInput.value;
+  console.log(userMail);
+  let isValid = false;
+  for (i = 0; i < dbMail.length; i++) {
+    if (userMail === dbMail[i]) {
+      isValid = true;
+    }
+  }
+  if (isValid) {
+    alert("sei dentro");
+  } else {
+    alert("sei fuori");
+  }
+});
