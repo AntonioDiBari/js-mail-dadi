4dad64f2a735f2517492ffb1344b3763b33c1378
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-13 12:51:28 +0100

    fix: output result with text and colour feat: userMail input controls

--- js-mail/js/main.js
+++ js-mail/js/main.js
@@ -17,17 +17,28 @@ const result = document.getElementById("result");
 let userMail;
 
 validateBtn.addEventListener("click", function () {
-  userMail = userMailInput.value;
-  console.log(userMail);
-  let isValid = false;
-  for (i = 0; i < dbMail.length; i++) {
-    if (userMail === dbMail[i]) {
-      isValid = true;
+  userMail = userMailInput.value.trim();
+  if (userMail && userMail.includes("@") && userMail.includes(".com")) {
+    let isValid = false;
+    for (i = 0; i < dbMail.length; i++) {
+      console.log(
+        "il ciclo è stato ripetuto per " + i + " volte prima di uscire."
+      );
+      if (userMail === dbMail[i]) {
+        isValid = true;
+        i =
+          dbMail.length; /* RIASSEGNO L'INDICE PER TERMINARE IL CICLO DATO CHE NON SERVE FARLO GIRARE ULTERIORMENTE */
+      }
     }
-  }
-  if (isValid) {
-    alert("sei dentro");
+    if (isValid) {
+      result.innerText = `Accesso consentito, Benvenuto ${userMail}`;
+      result.className = "text-success";
+    } else {
+      result.innerText = "Accesso negato";
+      result.className = "text-danger";
+    }
   } else {
-    alert("sei fuori");
+    result.innerText = "Inserisci una mail valida";
+    result.className = "text-warning";
   }
 });
